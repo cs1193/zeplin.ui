7218df74bfde861c59c233ad6cc4af8e69942d56
Chandresh Rajkumar Manonmani <cs1193@gmail.com>
2018-01-22 23:53:08 +0530

    Fixed Navigation, Board Editor Added

--- source/board/board.service.js
+++ source/board/board.service.js
@@ -4,11 +4,26 @@ export default class BoardService {
   static NAME: string = 'BoardService';
 
   /* @ngInject */
-  constructor($log, $q, $http, BOARD_ENDPOINT) {
+  constructor($log, $q, $http, GET_BOARDS_ENDPOINT, BOARD_ENDPOINT) {
     this.$log = $log;
     this.$q = $q;
     this.$http = $http;
+    this.GET_BOARDS_ENDPOINT = GET_BOARDS_ENDPOINT;
     this.BOARD_ENDPOINT = BOARD_ENDPOINT;
+  }
+
+  getBoards() {
+    const deferred = this.$q.defer();
+
+    this.$http.get(`${this.GET_BOARDS_ENDPOINT}`).then((response) => {
+      if (response && response.data && (response.status === 200 || response.status === 201)) {
+        deferred.resolve(response.data);
+      }
+    }, (error) => {
+      deferred.reject(error);
+    });
+
+    return deferred.promise;
   }
 
   getBoardById(id) {
